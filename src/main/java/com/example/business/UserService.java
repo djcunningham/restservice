6b1d929944cd03a54126ee8021bae290b72ddee3
deric.cunningham <deric.cunningham@gmail.com>
2020-01-28 13:44:35 -0500

    test dev commit

--- src/main/java/com/example/business/UserService.java
+++ src/main/java/com/example/business/UserService.java
@@ -17,7 +17,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
-//This is a merge conflict comment
+//This is a comment to the dev branch
 @Service
 @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class UserService implements IUserService {
