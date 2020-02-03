dd09aa37e3d70e9fc76b74379c209387e51299e9
deric.cunningham <deric.cunningham@gmail.com>
2020-02-03 11:22:46 -0500

    Added http tests for controller endpoints

--- src/test/java/com/example/restservice/RestserviceApplicationTests.java
+++ src/test/java/com/example/restservice/RestserviceApplicationTests.java
@@ -5,13 +5,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 public class RestserviceApplicationTests {
 
 	@Autowired
 	private UserController controller;
-
 
 	@Test
 	void contextLoads() {
